a6e555e7be2a22513e92cc20720cfc9670cf74a9
yasu71 <yasuyo71@gmail.com>
2020-03-24 22:56:46 +0000

    refactor jQuary code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,13 @@
 $(document).ready(() => {
+  let counterValue = $(".counter").val();
   $("textarea").on("keyup", function() {
-    let maxLength = 140;
-    // let length = 1;
     let length = $(this).val().length;
-    length = maxLength - length;
-    let remainNumber = $(".button-counter-container > output");
-    if (length < 0) {
-      remainNumber.text(length);
-      remainNumber.css("color", "#ff0000"); 
-    } else if (length >= 0){
-      remainNumber.text(length); 
-      remainNumber.css("color", "#545149"); 
-    }
+    let counter = $(".counter")
+    $(".counter").val(counterValue - length);
+    if (length < 140){
+      counter.css("color", "#545149"); 
+    } else if (length >= 140) {
+      counter.css("color", "#ff0000"); 
+    } 
   })
 });
