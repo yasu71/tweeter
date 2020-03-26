62e2f0730131fbb652a5e165f3ed2582bb39139c
yasu71 <yasuyo71@gmail.com>
2020-03-26 03:15:13 +0000

    add a form submission using jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -31,7 +31,7 @@ const data = [
 ];
 
 const renderTweets = function(tweets) {
-  const $tweetsContainer = $('#tweets-container');
+  const $tweetsContainer = $("#tweets-container");
   for (const tweet of tweets) {
     const $tweet = createTweetElement(tweet);
     $tweetsContainer.append($tweet);
@@ -39,57 +39,50 @@ const renderTweets = function(tweets) {
 };
 
 const createTweetElement = function(tweet) {
-  const $img = $('<img>').attr('src', tweet.user.avatars);
-  const $profileImage = $('<div>').addClass('profile-image').append($img);
-  const $profileName = $('<div>').addClass('profile-name').text(tweet.user.name);
-  const $tweetProfile = $('<div>').addClass('tweet-profile').append($profileImage, $profileName);
-  const $tweetUserName = $('<div>').addClass('user-name').text(tweet.user.handle);
-  const $header = $('<header>').append($tweetProfile, $tweetUserName);
-  const $tweetContent = $('<div>').addClass('tweet-content').text(tweet.content.text);
-  const $firstIcon = $('<i>').addClass('fab fa-font-awesome-flag');
-  const $firstList = $('<li>').append($firstIcon);
-  const $secondIcon = $('<i>').addClass('fas fa-retweet');
-  const $secondList = $('<li>').append($secondIcon);
-  const $thirdIcon = $('<i>').addClass('fas fa-heart');
-  const $thirdList = $('<li>').append($thirdIcon);
-  const $ul = $('<ul>').append($firstList, $secondList, $thirdList)
-  const $iconDiv = $('<div>').append($ul);
-  let $createdAt = $('<div>').addClass('posted-date');
+  const $img = $("<img>").attr("src", tweet.user.avatars);
+  const $profileImage = $("<div>").addClass("profile-image").append($img);
+  const $profileName = $("<div>").addClass("profile-name").text(tweet.user.name);
+  const $tweetProfile = $("<div>").addClass("tweet-profile").append($profileImage, $profileName);
+  const $tweetUserName = $("<div>").addClass("user-name").text(tweet.user.handle);
+  const $header = $("<header>").append($tweetProfile, $tweetUserName);
+  const $tweetContent = $("<div>").addClass("tweet-content").text(tweet.content.text);
+  const $firstIcon = $("<i>").addClass("fab fa-font-awesome-flag");
+  const $firstList = $("<li>").append($firstIcon);
+  const $secondIcon = $("<i>").addClass("fas fa-retweet");
+  const $secondList = $("<li>").append($secondIcon);
+  const $thirdIcon = $("<i>").addClass("fas fa-heart");
+  const $thirdList = $("<li>").append($thirdIcon);
+  const $ul = $("<ul>").append($firstList, $secondList, $thirdList)
+  const $iconDiv = $("<div>").append($ul);
+  let $createdAt = $("<div>").addClass("posted-date");
   let date = new Date();
   let currentDate = date.getTime();
   let dateDifference = Math.floor((currentDate - tweet.created_at)/1000/60/60/24);
   $createdAt.text(`${dateDifference} days ago`);
-  const $footer = $('<footer>').append($createdAt, $iconDiv);
-  const $article = $('<article>').append($header, $tweetContent, $footer);
+  const $footer = $("<footer>").append($createdAt, $iconDiv);
+  const $article = $("<article>").append($header, $tweetContent, $footer);
 
-  const $form = $('form');
-  const $tweetButton = $('#submit-tweet');
-
-  // $.ajax({
-  //   type: 'GET',
-  //   url: '/api/posts',
-  //   success: (data) => { console.log(data) },
-  //   error: () => { console.error('something bad happened') }
-  // });
-
-  $tweetButton.click(() => {
-    $.getJSON('/tweet') 
-      .then((data) => {
-        console.log(data);
-      })
-  });
-
-  // $.getJSON(`/${tweet.user.name}`)
-  // .then((user) => {
-  //   const $footer = $('<footer>').text(`${user.name} (${user.email})`);
-  //   $article.append($header, $body, $footer);
-  //   $postContainer.append($article);
-  // });
   
   return $article;
-  
-}
+};
 
 renderTweets(data);
+
+const $form = $("form");
+
+$form.on("submit", () => {
+  console.log(event);
+  event.preventDefault();
+  const formData = $form.serialize();
+
+  $.post("/tweets", formData)
+    .then((res) => {
+      $("#tweet-text").val();
+      console.log($("#tweet-text").val())
+    });
+});
+
+
+
 
 });
